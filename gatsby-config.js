6dc59ec2bde3803c513956375778271168f49cc4
NTN1789 <natanalmeida040@gmail.com>
2023-04-24 11:56:43 -0300

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,7 +9,7 @@ module.exports = {
   plugins: [{
     resolve: 'gatsby-source-datocms',
     options: {
-      "apiToken": ""
+      "apiToken": "ccf0d6dc246217e25250855b902fa4"
     }
   }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", "gatsby-plugin-styled-components"]
 };
